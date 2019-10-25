b0e6565e3ab783fc161457f644a7fb409f38c0e4
HaoNShi <37909396+HaoNShi@users.noreply.github.com>
2019-10-25 15:01:17 +0800

    新增 调整播放页的导航栏和关灯按钮的层级

--- M站优化.user.js
+++ M站优化.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         M站优化
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  优化bimibimi，去广告，允许选中和右键、还原鼠标样式
+// @version      0.3
+// @description  优化bimibimi，去广告，允许选中和右键，还原鼠标样式等
 // @author       HaoNShi
 // @match        http://www.bimibimi.tv/*
 // @grant        none
@@ -16,21 +16,32 @@
     $(document).ready(function() {
         $("div[style]").each(function() {
             if ($(this).css("z-index") == "2147483647") {
-                $(this).css("display","none");
+                $(this).remove();
             }
         })
     });
 
-    $("div[class='tuiguang']").remove();
+    $("div[class = 'tuiguang']").remove();
 
     // 允许选中和右键
-    document.oncontextmenu=function(e){return true;};
-    document.onselectstart=function(e){return true;};
+    document.oncontextmenu = function(e) {
+        return true;
+    };
+    document.onselectstart = function(e) {
+        return true;
+    };
 
     // 还原鼠标样式
     document.body.style.cursor = "default";
-    $("a").hover(function(){
+    $("a").hover(function() {
         $("a").css("cursor","pointer");
     })
+
+    // 调整播放页的导航栏和关灯按钮的层级
+    $(document).ready(function() {
+        $("ul").each(function() {
+            $(this).css("z-index", "9999");
+        })
+    });
 
 })();
