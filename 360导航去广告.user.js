2a3e5f31fe5142ef6052fa46f3e9c5be452d9d18
HaoNShi <37909396+HaoNShi@users.noreply.github.com>
2019-12-19 17:27:42 +0800

    Update 360导航去广告.user.js
    优化 识别和删除更多的广告位
    优化 修改脚本执行逻辑，现在可以删除延迟加载的广告

--- 360导航去广告.user.js
+++ 360导航去广告.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         360导航去广告
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  删除360导航部分广告
 // @author       HaoNShi
 // @match        *://hao.360.com/*
@@ -13,23 +13,30 @@
     'use strict';
 
     // Your code here...
-    var loadTime = 2000; // 网站的加载时间，默认2s，可根据实际网络调整
+    var refreshTime = 1000; // 检测广告的刷新时间
 
-    setTimeout(function(){
+    setInterval(function(){
         $("#top-full-column-tips").remove(); // 顶部广告
         $("#doc").css("top", "0"); // 去除顶部广告后上移body
 
         $("#activity").remove(); // 左侧广告
-        $(".textlink_ad_icon").parent().remove(); // 左侧热点中的广告条目
-        $(".ad-textlink").parent().remove(); // 左侧猜你爱看中的广告条目
+        $(".textlink_ad_icon").parent().remove(); // 热点广告条目
+        $(".ad-textlink").parent().remove(); // 猜你爱看广告条目
 
-        $("#bottom_hotsite").remove(); // 主栏广告
-        $(".ad-top").remove(); // 发现你喜欢
-        $(".ad.infoflow-guess-mv-ad").parent().parent().parent().remove(); // 快资讯中的广告
+        $("#bottom_hotsite").remove(); // 类导航栏广告
+        $(".ad-top").remove(); // 删除发现你喜欢
+        $(".ad.infoflow-guess-mv-ad").parent().parent().parent().remove(); // 快资讯广告
+        $(".infoflow-item.mv").remove(); // 娱乐头条广告
+        $(".mv").remove(); // 游戏攻略广告
+        $(".clearfix.feed-mv.js-mv-infoflow-item").remove(); // 娱乐沸点广告
+        $(".feed-item.feed-mv.js-mv-infoflow-item").remove(); // 推荐广告
 
         $(".plane-hd.plane-adsvc").remove(); // 右侧导航栏广告
+        $("#festival_float_bottom").remove(); // 底部横幅广告
         $("#popup_news").remove(); // 下方悬浮广告
+        $(".festival_float_bottom-leftBottom").remove();// 左下角广告
         $("#large2small").remove(); // 右下角广告
 
-    }, loadTime);
+    }, refreshTime);
+
 })();
