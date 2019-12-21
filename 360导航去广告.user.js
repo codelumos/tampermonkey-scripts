d6b64b0ef5ae9d1c9972a65df267b785be2905f3
HaoNShi <37909396+HaoNShi@users.noreply.github.com>
2019-12-21 17:26:26 +0800

    Update 360导航去广告.user.js
    优化 识别和删除更多的广告位

--- 360导航去广告.user.js
+++ 360导航去广告.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         360导航去广告
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  删除360导航部分广告
+// @version      1.0
+// @icon         http://hao.360.com/favicon.ico
+// @description  删除360导航中被标记为广告的部分
 // @author       HaoNShi
 // @match        *://hao.360.com/*
 // @grant        none
@@ -21,8 +22,13 @@
         $("li[notice-ad = 'true']").remove(); // 消息广告
 
         $("#activity").remove(); // 左侧广告
+        $(".ad").parent().remove(); // 左侧热点图片
         $(".textlink_ad_icon").parent().remove(); // 热点广告条目
         $(".ad-textlink").parent().remove(); // 猜你爱看广告条目
+
+        $("#channel-game").remove(); // 左侧游戏广告栏
+        $("#channel-mall").remove(); // 左侧购物广告栏
+        $("#channel-car").remove(); // 左侧汽车广告栏
 
         $("#bottom_hotsite").remove(); // 类导航栏广告
         $(".ad-top").remove(); // 删除发现你喜欢
@@ -31,8 +37,10 @@
         $(".mv").remove(); // 游戏攻略广告
         $(".clearfix.feed-mv.js-mv-infoflow-item").remove(); // 娱乐沸点广告
         $(".feed-item.feed-mv.js-mv-infoflow-item").remove(); // 推荐广告
-        $(".gouwu-left").remove(); // 购物广告
-        $(".licai-right").remove(); // 金融广告
+
+        $("#service-gouwu").remove(); // 购物广告
+        $("#service-youxi").remove(); // 游戏广告
+        $("#service-licai").remove(); // 金融广告
 
         $(".plane-hd.plane-adsvc").remove(); // 右侧导航栏广告
         $("#festival_float_bottom").remove(); // 底部横幅广告
@@ -41,5 +49,10 @@
         $("#large2small").remove(); // 右下角广告
 
     }, refreshTime);
+
+    $(".notice-panel-count").text("(2)"); // 修改顶部消息数量
+    $(".plane-bd").find('li:eq(2)').remove(); // 右侧对应购物导航
+    $(".plane-bd").find('li:eq(2)').remove(); // 右侧对应游戏导航
+    $(".plane-bd").find('li:eq(3)').remove(); // 右侧对应理财导航
 
 })();
