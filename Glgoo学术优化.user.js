4cffbc4f3bac7122dd9a47601ab2a756f898b3ae
HaoNShi <37909396+HaoNShi@users.noreply.github.com>
2020-01-11 14:40:57 +0800

    Update Glgoo学术优化.user.js
    优化 logo大小和位置
    优化 识别更多的域名

--- Glgoo学术优化.user.js
+++ Glgoo学术优化.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Glgoo学术优化
 // @namespace    https://github.com/HaoNShi/Tampermonkey_Scripts
-// @version      0.1
+// @version      0.2
 // @icon         https://xs.xiayige.org/favicon.ico
-// @description  Glgoo学术搜索去广告，搜索栏增加logo
+// @description  Glgoo学术搜索去广告和其他一系列优化
 // @author       HaoNShi
 // @match        *://xs.xiayige.org/*
 // @match        *://scholar.123admin.com/*
+// @match        *://xueshu.123admin.com/*
 // @match        *://www.gugexueshu.com/
 // @grant        none
 // @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
@@ -16,9 +17,12 @@
     'use strict';
 
     // Your code here...
+    // 去广告
     $(".adsbygoogle").remove();
     $("#gs_hdr_act").remove();
-    $("#gs_hdr_lgo").append('<img id="logo" src="//gstatic.kkssl.com/logo.png" width="158" height="46"></img>');
-    $("#logo").css({"margin":"10px 0px 0px 10px"});
+    
+    // 搜索栏增加logo
+    $("#gs_hdr_lgo").append('<img id="logo" src="//gstatic.kkssl.com/logo.png" width="140"></img>');
+    $("#logo").css({"margin":"15px 0px 0px 15px"});
 
 })();
