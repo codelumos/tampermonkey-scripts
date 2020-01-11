a6521cca513a12e08f9adeab1cb51afc12a7563e
HaoNShi <37909396+HaoNShi@users.noreply.github.com>
2020-01-11 14:28:07 +0800

    Update M站优化.user.js
    新增 对齐提示条

--- M站优化.user.js
+++ M站优化.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         M站优化
 // @namespace    https://github.com/HaoNShi/Tampermonkey_Scripts
-// @version      1.2
+// @version      1.3
 // @icon         http://www.bimibimi.tv/favicon.ico
-// @description  优化bimibimi：去广告，允许选中和右键，还原鼠标样式等
+// @description  bimibimi去广告以及其他一系列优化
 // @author       HaoNShi
 // @match        *://www.bimibimi.tv/*
 // @grant        none
@@ -33,7 +33,7 @@
     // 还原鼠标样式
     document.body.style.cursor = "default";
     $("a").hover(function() {
-        $("a").css("cursor","pointer");
+        $("a").css("cursor", "pointer");
     })
 
     // 删除伪搜索历史框
@@ -41,5 +41,14 @@
 
     // 调整播放页的导航栏和关灯按钮的层级
     $(".wrapper > ul > li > ul").css("z-index", "999");
+
+    // 对齐提示条
+    $("div[style]").each(function() {
+        if ($(this).css("width") == "1155px") {
+            $(this).css("margin-left", "0px");
+            $(this).css("width", "1190px");
+        }
+    })
+
 
 })();
