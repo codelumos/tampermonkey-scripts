29f5c2c245878456acbdbd3423377c702672465c
HaoNShi <37909396+HaoNShi@users.noreply.github.com>
2020-10-31 16:17:51 +0800

    Update 快资讯去广告.user.js
    优化 识别和删除更多的广告位

--- scripts/快资讯去广告.user.js
+++ scripts/快资讯去广告.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         快资讯去广告
 // @namespace    https://github.com/HaoNShi/Tampermonkey_Scripts
-// @version      0.1
+// @version      1.0
 // @icon         https://p5.ssl.qhimg.com/t013ed96d9c4cf74064.png
 // @description  删除快资讯中的广告
 // @author       HaoNShi
@@ -13,17 +13,17 @@
     'use strict';
 
     // Your code here...
-    var loadTime = 1000;		// 延迟加载广告的加载时间
     var refreshTime = 1000;		// 反复加载广告的检测刷新时间
 
-    setTimeout(function(){
+    setInterval(function(){
         $("#as_shop_gg").remove(); // 主页购物广告
         $(".popupgg").remove(); // 详情页右下角广告
-    }, loadTime);
-
-    setInterval(function(){
+        $(".top-part").remove(); // 顶栏广告
         $("[data-ggclick='ad']").remove(); // 主页广告条目
+        $(".g4").remove(); // 横幅广告
         $("[data-ggsrc]").remove(); // 详情页广告条目
         $(".guessgg").remove(); // 详情页购物广告
+        $(".bkg-block").remove(); // 背景广告
+        $(".bkg-close").remove(); // 背景广告
     }, refreshTime);
 })();
