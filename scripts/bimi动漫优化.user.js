f7e19b278f84caa2496c0b8f4faac56a034c8fdc
HaoNShi <haonshi@qq.com>
2022-05-10 23:41:15 +0800

    一些优化

--- scripts/bimi动漫优化.user.js
+++ scripts/bimi动漫优化.user.js
@@ -1,29 +1,29 @@
 // ==UserScript==
 // @name         bimi动漫优化
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.2
 // @icon         https://www.bimiacg5.net/favicon.ico
-// @description  bimi动漫（M站）去广告以及其他一系列优化
+// @description  bimi动漫（M站）去广告以及一些其他优化
 // @author       CodeLumos
 // @homepageURL  https://github.com/codelumos/tampermonkey-scripts
 // @match        *://*.dodoge.me/*
 // @match        *://*.bimiacg.net/*
 // @match        *://*.bimiacg4.net/*
 // @match        *://*.bimiacg5.net/*
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
     'use strict';
 
     // 去广告
+    GM_addStyle("#HMcoupletDivleft {display: none;}"); // 左侧悬浮广告
+    GM_addStyle("#HMcoupletDivright {display: none;}"); // 右侧悬浮广告
+    GM_addStyle(".tuiguang {display: none;}"); // 横幅广告
     setInterval(function () {
         $("#HMRichBox").remove(); // 右下角广告
-        $("#HMcoupletDivleft").remove(); // 右下角广告
-        $("#HMcoupletDivright").remove(); // 右下角广告
         $("iframe").contents().find("#adv_wrap_hh").remove(); // 播放器暂停广告
     }, 500);
-    $(".tuiguang").remove();
 
     // 允许选中和右键
     document.oncontextmenu = function () {
